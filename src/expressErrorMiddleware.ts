f3f17a56d386ac0c62c713d14ac1025d7a043987
Snahier <hansalmeida@hotmail.com>
2021-06-25 16:14:52 -0300

    Create error middleware

---
+++ src/expressErrorMiddleware.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express"
+
+const expressErrorMiddleware = (
+  error: Error,
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  if (error instanceof Error)
+    return response.status(400).json({ error: error.message })
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  })
+}
+
+export { expressErrorMiddleware }
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
-import express from "express"
+import express, { NextFunction, Request, Response } from "express"
+import "express-async-errors"
 import "reflect-metadata"
 import "./database"
+import { expressErrorMiddleware } from "./expressErrorMiddleware"
 import { routes } from "./routes"
 
 const app = express()
 
 app.use(express.json())
 app.use(routes)
+app.use(expressErrorMiddleware)
 
 app.listen(3000, () => console.log("Running at http://localhost:3000"))
