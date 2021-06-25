0cf94c06c99e5bbdeaffb9ac1af1aefc5cdf0e1d
Snahier <hansalmeida@hotmail.com>
2021-06-25 16:44:26 -0300

    Create route createTag

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express"
+import { CreateTagController } from "./controllers/CreateTagController"
 import { CreateUserController } from "./controllers/CreateUserController"
 
 const routes = Router()
 
 const createUserController = new CreateUserController()
+const createTagController = new CreateTagController()
 
 routes.post("/users", createUserController.handle)
+routes.post("/tags", createTagController.handle)
 
 export { routes }
