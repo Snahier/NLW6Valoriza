054dbfe2758112b1427e5f0ccea7208e95f3a0b4
Snahier <hansalmeida@hotmail.com>
2021-06-25 15:59:33 -0300

    Add create user route

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from "express"
 import "reflect-metadata"
 import "./database"
+import { routes } from "./routes"
 
 const app = express()
 
-app.listen(3000, ()=> console.log("Running at http://localhost:3000"))
+app.use(routes)
+
+app.listen(3000, () => console.log("Running at http://localhost:3000"))
