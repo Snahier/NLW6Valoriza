5e39d6641c711137a8a27fe4ef51889d549138ae
Snahier <hansalmeida@hotmail.com>
2021-06-26 05:38:02 -0300

    Create authentication middleware

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,33 @@
+import { NextFunction, Request, Response } from "express"
+import { verify } from "jsonwebtoken"
+
+interface IPayload {
+  sub: string
+}
+
+const ensureAuthenticated = (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  const authToken = request.headers.authorization
+
+  if (!authToken) return response.status(401).end()
+
+  const [, token] = authToken.split(" ")
+
+  try {
+    const { sub } = verify(
+      token,
+      "85cbb6d35624bf9db788b77cfab9f73f"
+    ) as IPayload
+
+    request.user_id = sub
+
+    return next()
+  } catch (error) {
+    return response.status(401).end()
+  }
+}
+
+export { ensureAuthenticated }
