37c71d81832f6cc7d96d6d91c70a74ffdbcd45b2
Snahier <hansalmeida@hotmail.com>
2021-06-26 05:38:58 -0300

    Replace admin mock with JWT confirmation

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,13 +1,19 @@
 import { NextFunction, Request, Response } from "express"
+import { getCustomRepository } from "typeorm"
+import { UsersRepository } from "../repositories/UsersRepository"
 
-const ensureAdminMiddleware = (
+const ensureAdminMiddleware = async (
   request: Request,
   response: Response,
   next: NextFunction
 ) => {
-  const isAdmin = true
+  const { user_id } = request
 
-  if (isAdmin) return next()
+  const usersRepository = getCustomRepository(UsersRepository)
+
+  const { admin } = await usersRepository.findOne(user_id)
+
+  if (admin) return next()
 
   return response.status(401).json({ error: "Unauthorized" })
 }
